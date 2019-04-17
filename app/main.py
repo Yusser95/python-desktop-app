aef14d5c0ecd3dc50bc979b2d476b1342ff88ea9
AnasSakhnini <anas.sakhnini.256@gmail.com>
2019-04-16 19:01:15 -0700

    developed canave and Buttomn to upload and show image using openvv

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
 from tkinter import *
+import cv2
+from cv2 import imread
+import PIL.Image, PIL.ImageTk
+
+global photo
+global cv_image
+global entry_L
+global canvas
+def read_image():
+
+    cv_image = imread(entry_L.get())
+    photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_image))
+    canvas.create_image(0, 0, image=photo, anchor=NW)
+
 
 root = Tk()
 root.title('Model Definition')
@@ -20,10 +34,11 @@ center.grid(row=1, sticky="nsew")
 # create the widgets for the top frame
 model_label = Label(top_frame, text='Model Dimensions')
 entry_L = Entry(top_frame, background="orange",width=50)
+btn_image=Button(top_frame, text="open", command=read_image)
 
 # layout the widgets in the top frame
 entry_L.grid(row=0,column=0,sticky='nswe' )
-model_label.grid(row=0,column=1, columnspan=3,sticky='nswe')
+btn_image.grid(row=0,column=1, columnspan=3,sticky='nswe')
 
 top_frame.grid_rowconfigure(0, weight=1)
 top_frame.grid_columnconfigure(0, weight=1)
@@ -34,16 +49,13 @@ center.grid_rowconfigure(1, weight=1)
 center.grid_columnconfigure(0, weight=1)
 
 ctr_left = Frame(center, bg='blue', width=275, height=190,padx=3, pady=3)
-#ctr_mid = Frame(center, bg='yellow', width=250, height=190, padx=3, pady=3)
 ctr_right = Frame(center, bg='green', width=275, height=190, padx=3, pady=3)
-#ctr_left.place(relheight=1,relwidth=1,anchor='e')
-#ctr_right.place(relheight=1,relwidth=1,anchor='w')
-#ctr_left.grid(row=0, column=0, sticky="ns")
-#ctr_mid.grid(row=0, column=1, sticky="nsew")
-#ctr_right.grid(row=0, column=1, sticky="ns")
 
 ctr_left.pack(side=LEFT,fill='both', expand=True)
 ctr_right.pack(side = RIGHT,fill='both', expand=True)
+
+canvas = Canvas(ctr_left)
+canvas.pack(fill='both', expand=True)
 
 ctr_right_TOP= Frame(ctr_right, bg='green', width=275, height=95, padx=3, pady=3)
 ctr_right_BOTTOM = Frame(ctr_right, bg='red', width=275, height=95, padx=3, pady=3)
@@ -51,5 +63,34 @@ ctr_right_BOTTOM = Frame(ctr_right, bg='red', width=275, height=95, padx=3, pady
 ctr_right_TOP.pack(side=TOP,fill='both', expand=True)
 ctr_right_BOTTOM.pack(side = TOP,fill='both', expand=True)
 
+#reads an input image 
+ 
 
+# # Callback for the "Blur" button
+
+
+#     photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_image))
+#     canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
+
+# # find frequency of pixels in range 0-255 
+# histr = calcHist([cv_image],[0],None,[256],[0,256]) 
+# gray = cvtColor(cv_image, COLOR_BGR2GRAY)
+
+# # Get the image dimensions (OpenCV stores image data as NumPy ndarray)
+# height, width, no_channels = cv_image.shape
+# # Create a canvas that can fit the above image
+# canvas = tkinter.Canvas(window, width = width, height = height)
+# #canvas.pack()
+
+# # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
+# photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_image))
+
+# # Add a PhotoImage to the Canvas
+# canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
+# # Button that lets the user blur the image
+# btn_blur=tkinter.Button(top_frame, text="Blur", command=blur_image)
+# #btn_blur.pack(side=LEFT, expand=True)
+# #imshow('Original image',image)
+# #imshow('Gray image', gray)
+# Run the window loop
 root.mainloop()
